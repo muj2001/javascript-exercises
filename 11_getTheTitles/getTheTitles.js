1a1402c5f3a89d2b559451c562aea2df1acc379a
muj2001 <m.usman.jehangir@gmail.com>
2024-08-28 11:25:25 +0500

    Completed 11_getTheTitles exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let titles = [];
+    for (let i = 0; i < books.length; i++) {
+        // console.log(book);
+        titles.push(books[i].title);
+        // console.log(titles);
+    };
+    return titles;
 };
 
 // Do not edit below this line
