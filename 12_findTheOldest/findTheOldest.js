69b61e835c77a8e6b4a196c025217c5eed2af724
muj2001 <m.usman.jehangir@gmail.com>
2024-08-28 12:06:05 +0500

    Completed 12_findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let maxAge = 0;
+    let maxPerson = Object();
+    let currentYear = 2024;
+    for (let i = 0; i < people.length; i++) {
+        // let name = people[i].name;
+        let yearOfBirth = people[i].yearOfBirth;
+        let yearOfDeath = people[i].yearOfDeath;
+        if (yearOfDeath == undefined) {
+            yearOfDeath = currentYear;
+        }
+        let age = yearOfDeath - yearOfBirth;
+        // console.log(name, age);
+        if (age > maxAge) {
+            maxAge = age;
+            maxPerson = people[i];
+        }
+        // console.log("MAX", maxAge, maxName);
+    };
+    // console.log(maxName);
+    return maxPerson;
 };
 
 // Do not edit below this line
