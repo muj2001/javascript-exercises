8f9b47da3ee9923b70c106bbf74b68ce7f8d6b13
muj2001 <m.usman.jehangir@gmail.com>
2024-08-28 11:15:14 +0500

    Completed 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    text = text.toLowerCase();
+    text = text.split("")
+    text = text.filter((char) => {
+        return char != "." && char != "," && char != "!" && char != " ";
+    });
+    let left = 0;
+    let right = text.length - 1;
+    while (left < right) {
+        if (text[left] == text[right]) {
+            right -= 1;
+            left += 1;
+        } else {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
